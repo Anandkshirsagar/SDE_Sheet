d74ea102f56160f5bec73db31c97c38e6130baa4
Anandkshirsagar <119808083+Anandkshirsagar@users.noreply.github.com>
2024-01-06 03:20:09 +0000

    Add Some more feature

--- Pattern.java
+++ Pattern.java
@@ -25,9 +25,39 @@ public class Pattern {
             System.out.println();
         }
     }
+    
+    public static void triangle_122333(){
+        for(int i=0;i<=5;i++){
+        for(int j=0;j<i;j++){
+            System.out.print(i);
+        }
+        System.out.println();
+        }
+    }
+
+    public static void DecTraingle(){
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5-i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void DecTraingle123(){
+        for(int i=0;i<5;i++){
+            for(int j=1;j<5-i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         //rectangle();
         //triangle();
-        triangle_1234();
+        //triangle_1234();
+        //triangle_122333();
+       // DecTraingle();
+       DecTraingle123();
     }
 }
