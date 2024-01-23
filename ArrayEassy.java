ee91827b8a91c191dd96945671a3a9badb15e3ae
Anandkshirsagar <119808083+Anandkshirsagar@users.noreply.github.com>
2024-01-23 04:35:11 +0000

    Add function in ArrayEassy

--- ArrayEassy.java
+++ ArrayEassy.java
@@ -39,20 +39,62 @@ public class ArrayEassy {
         for (int i = 0; i < arr.length; i++) {
             hs.add(arr[i]);
         }
+        int len = hs.size();
+        int temp[] = new int[len];
+        int i = 0;
         for (int x : hs) {
-            System.out.print(x + " ");
+            temp[i++] = x;
+        }
+        for (int z : temp) {
+            System.out.println(z);
         }
 
     }
 
+    public static void leftRoteteByOne(int arr[]) {
+        int temp = arr[0];
+        int len = arr.length;
+        for (int i = 1; i < len; i++) {
+            arr[i - 1] = arr[i];
+        }
+        arr[len - 1] = temp;
+
+        for (int x : arr) {
+            System.out.println(x);
+        }
+
+    }
+
+    public static void rotateDplace(int arr[], int d) {
+        int len = arr.length;
+        d = d % len;
+        System.out.println(d + " d");
+        int temp[] = new int[d];
+        for (int i = 0; i < d; i++) {
+            temp[i] = arr[i];
+        }
+        for (int j = d; j < len; j++) {
+            arr[j - d] = arr[j];
+        }
+        int l = 0;
+        for (int k = len - d; k < len; k++) {
+            arr[k] = temp[l++];
+        }
+        for (int m : arr) {
+            System.out.println(m);
+        }
+    }
+
     public static void main(String[] args) {
-        // int arr[] = { 1, 2, 7, 55, 6 };
-        int arr[] = { 1, 2, 3, 3, 2, 1 };
+        int arr[] = { 1, 2, 7, 55, 6 };
+        // int arr[] = { 1, 2, 3, 3, 2, 1 }; // for check remove duplicate from arr
         // int arr[] = { 1, 2, 3, 4 };//for check ischeck function
         // System.out.println("Largest Number in the array " + largest(arr));
         // SecondLargest(arr);
         // System.out.println(ischeck(arr));
-        removeDup(arr);
+        // removeDup(arr);
+        // leftRoteteByOne(arr);
+        rotateDplace(arr, 3);
 
     }
 }
